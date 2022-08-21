469307af528e05b3fb847748405e94f258c4cc8e
YanivWein24 <yanivwein22@gmail.com>
2022-08-21 22:50:32 +0300

    Radix Sort

--- Sorting algorithms/Radix Sort.js
+++ Sorting algorithms/Radix Sort.js
@@ -87,3 +87,39 @@ console.log(mostDigits([1234, 56, 7])) // 4
 console.log(mostDigits([1, 1, 11111, 1])) // 5 
 console.log(mostDigits([12, 34, 56, 78])) // 2
 console.log(mostDigits([3, 78, 45236758, 123, 3754])) // 8
+
+
+//* Radix Sort Pseudocode:
+
+//* * Define a function that accepts a list of numbers
+//* * Figure out how many digits the largest number has (using 'mostDigits()')
+//* * Loop from k = 0 up to this largest number of digits
+//* * For each iteration of the loop:
+//*     * Create buckets for each digit (0-9)
+//*     * Place each number in the corresponding bucket based on its k'th digit.
+//* * Replace our existing array with values in our buckets, starting with 0 and going up to 9.
+//* * Return the sorted array at the end!
+
+
+const radixSort = (nums) => {
+    let maxDigitCount = mostDigits(nums)
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({ length: 10 }, () => [])
+        // Creating an array of 10 empty sub-arrays : [[], [], [], [], [],[], [], [], [], []]
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k)
+            // if nums[i] = 1234 and k = 0, then = getDigit(1234,0) = 4 (the 0th index from right)
+            digitBuckets[digit].push(nums[i]);
+            // so we will push nums[i] to digitBuckets[4]
+        }
+        nums = [].concat(...digitBuckets)
+        // if we won't use the spread operator,our array will end up like this: [[1],[2],[3]] instead of [1,2,3].
+    }
+    return nums
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]))  // [ 12, 23, 345, 2345, 5467, 9852 ]
+console.log(radixSort([234, 15, 67, 0, 2345, 9852]))  // [ 0, 15, 67, 234, 2345, 9852 ]
+
+//! Time Complexity: O(n * k) - the number of elements in the array times the number of digits in the largest numbers
+//! Space Complexity: O(n + k) - the number of elements in the array plus the number of digits in the largest numbers
