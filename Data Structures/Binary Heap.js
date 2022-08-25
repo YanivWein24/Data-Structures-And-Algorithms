75440b3aa91c4eb532e86334d39e8f838cda517f
YanivWein24 <yanivwein22@gmail.com>
2022-08-25 15:06:12 +0300

    Completed Binary Heaps

--- Data Structures/Binary Heap.js
+++ Data Structures/Binary Heap.js
@@ -2,7 +2,7 @@
 //? Very similar to a "Binary Search Tree", but with some different rules!
 //? In binary heaps, there are no 'left' and 'right' relationships!
 
-//* Each parent has AT MOST 2 child nodes.
+//* Each parent has AT MOST 2 children .
 //* * In a 'MaxBinaryHeap', parent nodes are always *larger* than child nodes,
 //*   but there are no guarantees between sibling nodes.
 //!   The root node is always the *largest* node.
@@ -64,8 +64,14 @@
 // if we're looking at the 5th index we have '25', which is the *child* of the node at index Math.floor((5-1)/2) ->
 // -> Arr[Math.floor((5-1)/2)] = Arr[Math.floor(2.5)] = arr[2] = 36
 
+//! Time Complexity:
+
+//! Insertion - O(log n)
+//! Removal - O(log n)
+//! Searching - O(n)
 
 //? Methods Pseudocode (for MaxBinaryHeap):
+//? insert(value):
 //? * Push the value into the end values property on the heap.
 //? * 'Bubble' the value up to it's correct spot (by comparing the value with the parent's value and swaping them accordingly).
 //? How To Bubble:
@@ -74,6 +80,19 @@
 //?   * Keep looping as long as the values of the parent nodes is less then the value of the element at the child index.
 //?     * Swap the value of the element at the parentIndex with the value of the element at the child index
 //?     * Set the index to be the parent index, and start over!
+
+//? extractMax():
+//? * Swap the FIRST value in the 'values' property with the LAST one.
+//? * Pop from the values property, so you can return the value at the end.
+//? * Have the new root "sink down" to the correct spot...
+//?   * Your parent index start at 0 (the root)
+//?   * Find the index of the left child: 2 * index + 1 (make sure it not out of bounds)
+//?   * Find the index of the right child: 2 * index + 2 (make sure it not out of bounds)
+//?   * If the left or right child is greater than the element... swap.
+//?     * if *both* left and right children are larger, swap with the largest child.
+//?   * The child index you swapped to now becomes the new parent index.
+//?   * Keep looping and swapping until neither child is larger than the element.
+//?   * Return the old root!
 
 
 class MaxBinaryHeap {
@@ -96,14 +115,58 @@ class MaxBinaryHeap {
             index = parentIndex
         }
     }
+    extractMax() {
+        const max = this.values[0]  // stores the 'old' max number
+        const end = this.values.pop()
+        if (this.values.length > 0) {  // if we just popped the *only* element, we will eventually return 'undefined'
+            this.values[0] = end
+            this.sinkDown()
+        }
+        return max
+    }
+    sinkDown() {
+        let index = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while (true) {
+            let leftChildIndex = 2 * index + 1
+            let rightChildIndex = 2 * index + 2
+            let leftChild, rightChild
+            let swap = null
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex]
+                if (leftChild > element) {
+                    swap = leftChildIndex
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex]
+                if ((swap === null && rightChild > element) || (swap === null && rightChild > leftChild)) {
+                    swap = rightChildIndex
+                }
+            }
+            if (swap === null) break
+            this.values[index] = this.values[swap] // 'swap' can be either the left or the right child
+            this.values[swap] = element
+            index = swap  // 'swap' can be either the left or the right child
+        }
+    }
 }
+
 
 let maxBinaryHeap = new MaxBinaryHeap()
 
-
 maxBinaryHeap.insert(55)
-console.log(maxBinaryHeap.values)  // [55, 39, 41, 18,27, 12, 33]
+console.log(maxBinaryHeap.values)
+// [55, 39, 41, 18,27, 12, 33] - 55 was inserted to the end, than bubbled up to the correct position.
 maxBinaryHeap.insert(1)
+// [55, 39, 41, 18,27, 12, 33, 1] - 1 was inserted to the end and remained there, since its the correct position.
 console.log(maxBinaryHeap.values)  // [55, 39, 41, 18,27, 12, 33,  1]
 maxBinaryHeap.insert(45)
 console.log(maxBinaryHeap.values)  // [55, 45, 41, 39, 2, 12, 33, 1, 18]
+// [55, 45, 41, 39, 2, 12, 33, 1, 18] - 45 was inserted to the end, than bubbled up to the correct position.
+
+console.log(maxBinaryHeap.extractMax())  // 55 - (the old root)
+console.log(maxBinaryHeap.extractMax())  // 45 - (the old root)
+console.log(maxBinaryHeap.extractMax())  // 39 - (the old root)
